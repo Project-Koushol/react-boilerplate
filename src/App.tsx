0600fc11c7e04390f65a37c72cbde10fbc13b4e1
19smabtahinoor <abtahi.educxy@gmail.com>
2025-06-01 18:15:39 +0600

    react-vite-ts-boilerplate v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">Welcome to React + Vite + TypeScript!</h1>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">
+        Welcome to React + Vite + TypeScript!
+      </h1>
+    </div>
+  );
 };
 
 export default App;
